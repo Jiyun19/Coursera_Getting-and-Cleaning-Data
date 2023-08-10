02a0a80d7e1d0f6c17ee70c6b1f90ea6bc567d8d
Ji <97488385+Jiyun19@users.noreply.github.com>
2023-08-10 16:34:30 +0900

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,9 @@
 library("dplyr")
 library("tidyr")
+library("psych")
 
 
-##Loading the data.
+##Loading the data
 
 x_test<-read.table(file="./test/X_test.txt")
 y_test<-read.table(file="./test/y_test.txt")
@@ -18,7 +19,7 @@ activity_name<-read.table(file="./activity_labels.txt")
 
 
 
-##Changing variable names.
+##Changing variable names
 names(x_test)<-x_label[,2]
 names(x_train)<-x_label[,2]
 names(subject_test)<-"test"
@@ -27,20 +28,20 @@ names(y_test)<-"activity"
 names(y_train)<-"activity"
 
 
-##Changing variable types (converting the 'activity' variable into a factor variable with understandable labels).
+##Changing variable types (converting the 'activity' variable into a factor variable with understandable labels)
 y_train$activity<-as.factor(y_train$activity)
 levels(y_train$activity)<-activity_name[,2]
 y_test$activity<-as.factor(y_test$activity)
 levels(y_test$activity)<-activity_name[,2]
 
-##Extracting specified variables.
+##Extracting specified variables
 extract_variables<-x_label[grep("mean\\()|std\\()",x_label[,2]),2]
 extract_variables
 x_train2<-x_train[,extract_variables]
 x_test2<-x_test[,extract_variables]
 
 
-##Merging training and test data into a single set, and adding an additional variable to differentiate between them.
+##Merging training and test data into a single set, and adding an additional variable to differentiate between them
 merged_train<-cbind(subject_train,x_train2,y_train)
 merged_test<-cbind(subject_test,x_test2,y_test)
 
@@ -51,7 +52,7 @@ first_set<-merged_all %>%
   drop_na(subject)%>%
   relocate(subject,set,activity)%>%arrange(subject,set)
 
-##Making variable names understandable.
+##Making variable names understandable
 
 names(first_set)<-gsub("-", "_", names(first_set))
 names(first_set)<-gsub("^t", "time_", names(first_set))
@@ -62,12 +63,15 @@ names(first_set)<-gsub("__", "_", names(first_set))
 names(first_set)<-tolower(names(first_set))
 names(first_set)
 
-##Summarizing data using means grouped by subjects and activities.
+##Summarizing data using means grouped by subjects and activities
 second_set<-first_set%>%group_by(subject,activity)%>% summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE)))
 head(first_set)
 head(second_set)
 
-##Saving data.
+##Saving data
 write.table(first_set, "1st_data.txt", row.names = FALSE)
 write.table(second_set, "2nd_data.txt", row.names = FALSE)
 
+##summarize
+write.table(describe(first_set)[,c("mean","min","max")], sep="\t","clipboard")
+                              
